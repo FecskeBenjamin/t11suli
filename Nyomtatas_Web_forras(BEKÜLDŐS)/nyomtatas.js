a668061fdfeb16c53a0e3801a26ccc04f777e2d4
Fecske Benjámin <saminiax@gmail.com.com>
2023-02-23 18:49:45 +0100

    nyomatatás web beküldös

---
+++ Nyomtatas_Web_forras(BEKÜLDŐS)/nyomtatas.js
@@ -0,0 +1,11 @@
+function kalkulal() {
+    //Űrlapadatok
+    const szelesseg = Number(document.getElementById("szelesseg").value);
+    const magassag = Number(document.getElementById("magassag").value);
+    const papir = document.getElementById('papirtipus').value;
+    //Számítások
+    let terulet = Math.round((szelesseg * magassag) / 10000);
+    let koltseg = terulet * papir;
+    //Megjelenítés
+    document.getElementById('valasz').style.visibility = "visible";
+}
